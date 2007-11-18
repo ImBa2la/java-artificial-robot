023a1f5a3d8b35a40a62963dadef61ebb066b719
astyskin <astyskin@c10e8109-7634-0410-b54f-c54c24ea6f33>
2007-11-18 14:03:34 +0000

    Documentation 2008 was added

--- CriteriaAnalysis/src/org/styskin/ca/model/Pair.java
+++ CriteriaAnalysis/src/org/styskin/ca/model/Pair.java
@@ -28,10 +28,6 @@ public class Pair<T1, T2> {
 		this.o2 = o2;
 	}
 	
-	public boolean equals(Pair p2) {
-		return o1.equals(p2.getFirst()) && o2.equals(p2.getSecond());
-	}
-
 	public String toString() {
 		return "[" + o1.toString() + "; " + o2.toString() + "]";
 	}
@@ -39,4 +35,38 @@ public class Pair<T1, T2> {
 	public static <T1,T2> Pair<T1, T2> makePair(T1 o1, T2 o2) {
 		return new Pair<T1, T2> (o1, o2);	
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((o1 == null) ? 0 : o1.hashCode());
+		result = prime * result + ((o2 == null) ? 0 : o2.hashCode());
+		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Pair other = (Pair) obj;
+		if (o1 == null) {
+			if (other.o1 != null)
+				return false;
+		} else if (!o1.equals(other.o1))
+			return false;
+		if (o2 == null) {
+			if (other.o2 != null)
+				return false;
+		} else if (!o2.equals(other.o2))
+			return false;
+		return true;
+	}
+	
+	
 }
