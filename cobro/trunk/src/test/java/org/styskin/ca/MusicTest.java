47cfef2bad8f89385b187a7ced1e3a16cf63092b
astyskin <astyskin@c10e8109-7634-0410-b54f-c54c24ea6f33>
2009-02-27 16:09:01 +0000

    Merging was removed

--- cobro/trunk/src/test/java/org/styskin/ca/MusicTest.java
+++ cobro/trunk/src/test/java/org/styskin/ca/MusicTest.java
@@ -31,7 +31,7 @@ public class MusicTest extends TestCase {
 		BasicConfigurator.configure();
 	}
 	
-	public void testMusic() throws Exception {
+	public void atestMusic() throws Exception {
 		Criteria music = CriteriaXMLParser.loadXML("cfg/music/music.res.xml");
 		logger.info("Optimization started");
 //		Optimize control = Optimize.getInput("cfg/music/music.tsv", music);
@@ -50,35 +50,17 @@ public class MusicTest extends TestCase {
 		logger.info("CrossValidation: " + musicCrossValidation.check() + ", " + musicCrossValidation.checkAbs());
 	}
 	
-	public void atestRanking() throws Exception {
-		SortedMap<String, Double> fixed = new TreeMap<String, Double>();
-		{
-			BufferedReader in = new BufferedReader(new FileReader("cfg/music/fixed.r.out"));
-			String line;
-			while ((line = in.readLine()) != null) {
-				StringTokenizer st = new StringTokenizer(line);
-				fixed.put(st.nextToken(), CriteriaXMLParser.FORMAT.parse(st.nextToken()).doubleValue());
-			}
-			in.close();
-		}
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("r.out")));		
-		IntegralCriteria music = CriteriaXMLParser.loadXML("music.res.xml");
+	public void testRanking() throws Exception {
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("r.tmp")));		
+		IntegralCriteria music = CriteriaXMLParser.loadXML("cfg/music/music.res.xml");
 		OptimizeInputFormat optimizeInputFormat = new OptimizeInputFormat(music);
 //		BufferedReader in = new BufferedReader(new FileReader("cfg/music/html.base"));
 		BufferedReader in = new BufferedReader(new FileReader("cfg/music/music.in"));
 		String line = null;
 		optimizeInputFormat.init(in.readLine());
-		outer: while( (line = in.readLine()) != null) {
+		while( (line = in.readLine()) != null) {
 			Pair<Double, double[]> pair = optimizeInputFormat.parseLine(line);
 			String url = line.substring(0, line.indexOf('\t'));
-			while(fixed.size() > 0 && fixed.firstKey().compareTo(url) <= 0) {
-				out.printf("%s\t%s\n", fixed.firstKey(), CriteriaXMLParser.FORMAT.format(fixed.get(fixed.firstKey())));
-				if(fixed.firstKey().equals(url)) {
-					fixed.remove(fixed.firstKey());
-					continue outer;					
-				}
-				fixed.remove(fixed.firstKey());
-			}
 			out.printf("%s\t%s\n", url, CriteriaXMLParser.FORMAT.format(music.getValue(pair.getSecond())));
 		}
 		in.close();
